b424402ddd90802072fa6dfea4a2244fe2086b60
DamyoungKim <damyoung1225@gmail.com>
2021-06-16 18:36:17 +0900

    백준 9935 문자열폭탄 (문자열, stack)

--- algorithm/src/net/acmicpc/baekjoon/Main_9935_문자열폭탄.java
+++ algorithm/src/net/acmicpc/baekjoon/Main_9935_문자열폭탄.java
@@ -3,8 +3,6 @@ package net.acmicpc.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Stack;
 
 public class Main_9935_문자열폭탄 {
@@ -12,63 +10,44 @@ public class Main_9935_문자열폭탄 {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		
 		String s1 = br.readLine();
 		String s2 = br.readLine();
 		
 		Stack<Character> stack = new Stack<>();
-		
-		for(int i = 0; i < s1.length(); i++) {
-			stack.add(s1.charAt(i));
-		}
-		
 		Stack<Character> temp = new Stack<>();
-		List<Character> list = null;
-		while(true) {
-			if(stack.size() == 0) {
-				break;
-			}
-			char lastChar = stack.pop();
-			
-			if(lastChar == s2.charAt(s2.length() - 1)) {
-				list = new ArrayList<>();
-				list.add(lastChar); // 같으니까 list에 넣는다
-				for(int i = s2.length() - 2 ; i >= 0; i--) {
-					if(stack.isEmpty()) break;
-					char curChar = stack.pop();
-					char checkChar = s2.charAt(i);
-					
-					if(curChar == checkChar) {
-						list.add(curChar); // 같으니까 list에 넣는다
-						
-						if(i == 0 && !temp.isEmpty()) {
-							for(int j = 0, size = temp.size(); j < size; j++) {
-								stack.add(temp.pop());
-							}
-						}
-					}else {
-						temp.add(lastChar);
-						list.add(curChar);
-						for(int j = list.size() - 1; j >= 1; j--) {
-							stack.add(list.get(j));
-						}
+		char lastChar = s2.charAt(s2.length() - 1);
+		for(int i = 0; i < s1.length(); i++) {
+			if(s1.charAt(i) == lastChar && stack.size()  >= s2.length() - 1) {
+				temp.clear();
+				temp.add(s1.charAt(i));
+				boolean check = false;
+				for(int j = s2.length() - 2; j >= 0; j--) {
+					char tempChar = stack.pop();
+					temp.add(tempChar);
+					if(tempChar != s2.charAt(j)) {
+						check = true;
 						break;
 					}
 				}
+				if(check) {
+					for(int j = 0, size = temp.size(); j < size; j++) {
+						stack.add(temp.pop());
+					}
+				}
 			}else {
-				temp.add(lastChar);
+				stack.add(s1.charAt(i));
 			}
 		}
-		
-		
-		if(temp.size() == 0) {
+		if(stack.isEmpty()) {
 			System.out.println("FRULA");
-		} else {
-			for(int i = 0, size = temp.size(); i < size; i++) {
-				System.out.print(temp.pop());
+		}else {
+			StringBuffer sb = new StringBuffer();
+			for(int i = 0; i < stack.size(); i++) {
+				sb.append(stack.get(i));
 			}
+			System.out.println(sb);
 		}
+		
 	}
 
 }
