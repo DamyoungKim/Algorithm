c4a90febd792271fc41c2105ae1ab537698b2cea
김담영 <damyoung1225@gmail.com>
2021-07-08 21:52:11 +0900

    백준 숨바꼭질4 bfs 이전 방문 인덱스 관리 배열로 변경

--- algorithm/src/net/acmicpc/baekjoon/Main_13913_숨바꼭질4.java
+++ algorithm/src/net/acmicpc/baekjoon/Main_13913_숨바꼭질4.java
@@ -1,14 +1,15 @@
 package net.acmicpc.baekjoon;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main_13913_숨바꼭질4 {
 	static int N, K, result;
-	static boolean[] visited = new boolean[100001];
-	static Queue<ArrayList<Integer>> q = new LinkedList<>();
+	static int[] visited = new int[100001];
+	static Queue<Integer> q = new LinkedList<>();
 	static StringBuffer sb = new StringBuffer();
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -25,60 +26,47 @@ public class Main_13913_숨바꼭질4 {
 			System.out.println(sb);
 		}else {
 			bfs();
+			int index = K;
+			Stack<Integer> stack = new Stack<>();
+			stack.add(K);
+			while(index != N) {
+				stack.add(visited[index]);
+				index = visited[index];
+			}
+			sb.append(result);
+			sb.append('\n');
+			while(!stack.empty()) {
+				sb.append(stack.pop() + " ");
+			}
+			System.out.println(sb);
 		}
 	}
 
 	private static void bfs() {
-		visited[N] = true;
-		ArrayList<Integer> tempList = new ArrayList<>();
-		tempList.add(N);
-		q.offer(tempList);
+		Arrays.fill(visited, -1);
+		visited[N] = 0;
+		q.offer(N);
 		while (!q.isEmpty()) {
 			int size = q.size();
 			for (int s = 0; s < size; s++) {
-				ArrayList<Integer> list = q.poll();
-				int listSize = list.size();
-				int pos = list.get(list.size() - 1);
+				int pos = q.poll();
 				if (pos == K) {
-					
-					sb.append(result);
-					sb.append('\n');
-					for (int i = 0; i < listSize; i++) {
-						sb.append(list.get(i));
-						sb.append(" ");
-					}
-					System.out.println(sb);
 					return;
 				}
 				
-				if(pos * 2 <= 100000 && !visited[pos * 2]) {
-					visited[pos * 2] = true;
-					tempList = new ArrayList<>();
-					for(int i = 0; i < listSize; i++) {
-						tempList.add(list.get(i));
-					}
-					tempList.add(pos * 2);
-					q.offer(tempList);
+				if(pos * 2 <= 100000 && visited[pos * 2] == -1) {
+					visited[pos * 2] = pos;
+					q.offer(pos * 2);
 				}
 				
-				if(pos + 1 <= 100000 && !visited[pos + 1]) {
-					visited[pos + 1] = true;
-					tempList = new ArrayList<>();
-					for(int i = 0; i < listSize; i++) {
-						tempList.add(list.get(i));
-					}
-					tempList.add(pos + 1);
-					q.offer(tempList);
+				if(pos + 1 <= 100000 && visited[pos + 1] == -1) {
+					visited[pos + 1] = pos;
+					q.offer(pos + 1);
 				}
 				
-				if(pos - 1 >= 0 && !visited[pos - 1]) {
-					visited[pos - 1] = true;
-					tempList = new ArrayList<>();
-					for(int i = 0; i < listSize; i++) {
-						tempList.add(list.get(i));
-					}
-					tempList.add(pos - 1);
-					q.offer(tempList);
+				if(pos - 1 >= 0 && visited[pos - 1] == -1) {
+					visited[pos - 1] = pos;
+					q.offer(pos - 1);
 				}
 			}
 			result++;
