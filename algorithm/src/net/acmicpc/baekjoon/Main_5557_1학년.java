747fae7dcf0406b93714369fcceb39cfca261fff
김담영 <damyoung1225@gmail.com>
2021-07-24 14:58:05 +0900

    백준 dp

--- algorithm/src/net/acmicpc/baekjoon/Main_5557_1학년.java
+++ algorithm/src/net/acmicpc/baekjoon/Main_5557_1학년.java
@@ -9,27 +9,27 @@ public class Main_5557_1학년 {
 	static int N;
 	static long result;
 	static int[] arr;
-	static int[] dp;
+	static long[][] dp;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		N = Integer.parseInt(br.readLine());
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		arr = new int[N];
-		dp = new int[N];
-		for(int i = 0; i < N; i++) {
+		arr = new int[N + 1];
+		dp = new long[N][21];
+		for(int i = 1; i < N + 1; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		System.out.println(result);
-	}
-	private static void solve(int cnt, int val) {
-		if(val < 0 || val > 20) return;
-		if(cnt == N - 2) {
-			if(arr[N - 1] == val) result++;
-			return;
+		dp[1][arr[1]] = 1;
+		for(int i = 2; i < N; i++) {
+			for(int j = 0; j < 21; j++) {
+				int a = j + arr[i];
+				int b = j - arr[i];
+				if(a <= 20) dp[i][j] += dp[i - 1][a];
+				if(b >= 0) dp[i][j] += dp[i - 1][b];
+			}
 		}
-		solve(cnt + 1, val + arr[cnt + 1]);
-		solve(cnt + 1, val - arr[cnt + 1]);
+		System.out.println(dp[N - 1][arr[N]]);
 	}
 }
