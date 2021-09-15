030c2cd26f955c06dce44bb284c5cb920e8f1084
김담영 <damyoung1225@gmail.com>
2021-09-15 22:30:31 +0900

    백준 완탐 다른 방식 방문체크로 변경

--- algorithm/src/net/acmicpc/baekjoon/Main_1062_가르침.java
+++ algorithm/src/net/acmicpc/baekjoon/Main_1062_가르침.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
 
 public class Main_1062_가르침 {
 	static int N, K, max;
-	static int[][] arr;
+	static String[] arr;
 	static int[] selected;
 	static boolean[] visited;
 	public static void main(String[] args) throws Exception{
@@ -14,52 +14,49 @@ public class Main_1062_가르침 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		arr = new int[N]['z' - 'a' + 2];
+		
+		arr = new String[N];
 		selected = new int[K];
 		visited = new boolean['z' - 'a' + 1];
 		for (int i = 0; i < N; i++) {
-			String s = br.readLine();
-			arr[i]['z' - 'a' + 1] = s.length();
-			for (int j = 0; j < s.length(); j++) {
-				arr[i][s.charAt(j) - 'a']++;
-			}
+			arr[i] = br.readLine();
 		}
+		if(K < 5) {
+			System.out.println(0);
+			return;
+		}
+		K -= 5;
 		visited['a' - 'a'] = true;
 		visited['n' - 'a'] = true;
 		visited['t' - 'a'] = true;
 		visited['i' - 'a'] = true;
 		visited['c' - 'a'] = true;
-		if(K < 5) {
-			System.out.println(0);
-			return;
-		}
-		selected[0] = 'a';
-		selected[1] = 'n';
-		selected[2] = 't';
-		selected[3] = 'i';
-		selected[4] = 'c';
-		solve(5, 'b');
+		solve(0, 'b');
 		System.out.println(max);
 	}
 	private static void solve(int cnt, int start) {
 		if(cnt == K) {
 			int count = 0;
 			for (int i = 0; i < N; i++) {
-				int sum = 0;
-				for(int j = 0; j < K; j++) {
-					sum += arr[i][selected[j] - 'a'];
+				boolean check = true;
+				for (int j = 0; j < arr[i].length(); j++) {
+					if(!visited[arr[i].charAt(j) - 'a']) {
+						check = false;
+						break;
+					}
 				}
-				if(sum == arr[i]['z' - 'a' + 1]) {
-					count++;
-				}
+				
+				if(check) count++;
 			}
 			max = Math.max(max, count);
 			return;
 		}
 		for (int i = start; i <= 'z'; i++) {
 			if(visited[i - 'a']) continue;
+			visited[i - 'a'] = true;
 			selected[cnt] = i;
 			solve(cnt + 1, i + 1);
+			visited[i - 'a'] = false;
 		}
 	}
 }
